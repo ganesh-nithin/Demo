e70c4ac1ce8939ec68cae03435853d1aea9514d5
Sarvabhotla G. Nithin <sarvabhotla.g@solitontech.com>
2023-07-03 18:26:05 +0530

    "Updated"

--- angular-practice/src/app/hide-after.directive.ts
+++ angular-practice/src/app/hide-after.directive.ts
@@ -6,26 +6,38 @@ import {
   ViewContainerRef,
 } from '@angular/core';
 
+class HideAfterContext {
+  public hideAfter = 0;
+  public counter = 0;
+}
+
 @Directive({
   selector: '[hideAfter]',
 })
 export class HideAfterDirective implements OnInit {
   @Input('hideAfter')
-  delay = 0;
+  set delay(value: number | null) {
+    this._delay = value ?? 0;
+    this.context.hideAfter = this._delay / 1000;
+  }
+  private _delay = 0;
 
   @Input('hideAfterLater')
   placeholderRef: TemplateRef<any> | null = null;
+
+  private context = new HideAfterContext();
+
   constructor(
     private viewContainerRef: ViewContainerRef,
     private templateRef: TemplateRef<any>
   ) {}
 
   ngOnInit(): void {
-    this.viewContainerRef.createEmbeddedView(this.templateRef);
+    this.viewContainerRef.createEmbeddedView(this.templateRef, this.context);
     setTimeout(() => {
       this.viewContainerRef.clear();
       if (this.placeholderRef)
         this.viewContainerRef.createEmbeddedView(this.placeholderRef);
-    }, this.delay);
+    }, this._delay);
   }
 }
